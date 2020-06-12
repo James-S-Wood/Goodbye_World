4cdd52a610df444a387abe3fe2f266340a7a9e0b
James Wood <JSWood@live.com>
2020-06-11 23:34:17 -0400

    Initial commit

---
+++ src/com/example/idea/Main.java
@@ -0,0 +1,11 @@
+package com.example.idea;
+
+// Author: James S Wood 06/11/2020
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Goodbye World");  // Prints Goodbye World
+    }
+}
